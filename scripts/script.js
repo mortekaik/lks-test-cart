8d271c385bce42ae00847b1fe2c68c43920e5bf3
mortekaik <astral.eskimo@gmail.com>
2016-07-07 20:20:03 +0400

    added modal cart window (test)

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
-	var count = $('#count').text();
-	var sum = $('#sum').text();
-	console.log(count, sum);
-	if (sum < 1 || count < 1) {
-		$('.item-count').fadeOut();
-		$('.item-sum').fadeOut();
-	}
-	$('.button').on('click', function() {
-		// var productId = ($(this).attr('id')).split('_')[1];
+$(document).ready(function () {
+	var btnDeals = $('.button');
+
+	$(btnDeals).on('click', addToCart); // добавление товара в корзину по клику
+	$('#cart-slot').on('click', moveToCart);
+
+	/* функция добавления кол-ва товара и суммы в корзину */
+	function addToCart(btnDeals) {
+		// var productId = ($(btnDeals).attr('id')).split('_')[1];
 		var productId = ($(this).attr('id')).slice(8);
 		console.log(productId);
 		var productPrice = ($(this).prev('.price').text()).slice(0, -2);
 		console.log(productPrice);
+
 		$.ajax({
 			url: 'samplehtml.html',
 			type: 'POST',
@@ -20,21 +20,53 @@ $(document).ready(function() {
 				productPrice: productPrice
 			},
 			success: function (data) {
-				$('.empty-cart').html(data);
+				if (data == '0') {
+					$('#cart-slot').html('Пусто');
+				} else {
+					$('#cart-slot').html(data);
+				}
 			},
 			error: function() {
-					alert('Something wrong...')
+				alert('Something wrong...');
 			}
 		});
-			// success: function (data) {
-				// if (data) {
-				// 	$('.empty-cart').fadeOut();
-				// 	$('.item-count').fadeIn();
-				// 	$('.item-sum').fadeIn();
-				// }
-			// },
-			// error: function() {
-			// 	alert("Something wrong ...");
-			// }
+	}
+	
+	/* функция для открытия корзины в отдельном окне */
+	function moveToCart() {
+		$.ajax({
+			url: 'samplehtml.html',
+			type: 'POST',
+			success: function(data) {
+				if (data !== 0) {
+					$('#modal_cart').append(data);
+					loadCart();
+				}	
+			},
+			error: function() {
+				alert('something error...');
+			}
 		});
+	}
+
+	function loadCart() { // переход в окно корзины
+		$('body').addClass('lock'); // лочим скролл у body
+		$('#overlay').fadeIn(400, // пoкaзывaем пoдлoжку 
+		 	function(){ // пoсле выпoлнения предыдущей aнимaции
+				$('#modal_cart') 
+					.css('display', 'block') // показываем окно
+					.animate({opacity: 1, top: '50%'}, 200); // прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+		});
+	};
+	/* Зaкрытие мoдaльнoгo oкнa, тo же сaмoе в oбрaтнoм пoрядке */
+	$('#modal_cart_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
+		$('#modal_cart')
+			.animate({opacity: 0, top: '45%'}, 200,  // меняем прoзрaчнoсть нa 0 и двигaем oкнo вверх
+				function(){ // пoсле aнимaции
+					$(this).css('display', 'none'); // скрываем окно;
+					$('#overlay').fadeOut(400); // скрывaем пoдлoжку
+				}
+			);
+		$('body').removeClass('lock'); // разлочим скроллы у body
+	});
 });
